b4c7cc1e7ba5aaf25d864ddeb61e2b9675778579
nileshpratap <nileshpratap03@gmail.com>
2023-01-31 00:38:55 +0530

    database added succefully

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// connect database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
