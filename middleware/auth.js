3ee8743daf2cbba8f21dc23353a79d95d7cc53e8
nileshpratap <nileshpratap03@gmail.com>
2023-01-31 22:43:13 +0530

    Authentication feature completed. (created as a middleware)

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+// middleware has access to req, res
+
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function (req, res, next) {
+  //get token from the header
+  const token = req.header("x-auth-token");
+
+  // check if taken is not there
+  if (!token) {
+    return res.status(401).json({ msg: "No token, Authorization denied." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    req.user = decoded.user;
+
+    // call next() after all the middleware statements.
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: "Token is not valid." });
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,79 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const auth = require("../middleware/auth.js");
+const User = require("../models/User.js");
 
 const router = express.Router();
 
 // @route GET api/auth
 // @desc GET logged in user
 // @access Private
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
+router.get("/", auth, async (req, res) => {
+  // res.send("Get logged in user");
+
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server Error.");
+  }
 });
 
 // @route POST api/auth
 // @desc Auth user & get token
 // @access Public
-router.post("/", (req, res) => {
-  res.send("Log in user");
-});
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required."),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+      if (!user) {
+        return res.status(400).json({ msg: "invalid credentials" });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "invalid credentials" });
+      }
+
+      // new user added in the DB, now send client a json web token to log him in, protectedly.
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+
+        // we need a middleware to add this token in the header for account access.
+      );
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("server error");
+    }
+  }
+);
 
 module.exports = router;
