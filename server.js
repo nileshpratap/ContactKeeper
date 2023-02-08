e3869d8a99d310fe56ef53a42074276d09b848d4
nileshpratap <nileshpratap03@gmail.com>
2023-02-08 21:25:15 +0530

    added deploy script.

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ app.use(cors());
 // : true to use more powerful json parser
 app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 5000;
-
 app.get("/", (req, res) => {
   res.json();
 });
@@ -24,6 +22,18 @@ app.get("/", (req, res) => {
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
