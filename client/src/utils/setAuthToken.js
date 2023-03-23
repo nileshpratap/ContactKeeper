55502573f3b1c08d8e51a172723a356c157abf95
Nilesh Pratapwar <78169856+nileshpratap@users.noreply.github.com>
2023-03-23 17:28:35 +0530

    Update setAuthToken.js

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+axios.defaults.baseURL = "https://coult.onrender.com";
 const setAuthToken = (token) => {
   if (token) {
     axios.defaults.headers.common["x-auth-token"] = token.toString();
