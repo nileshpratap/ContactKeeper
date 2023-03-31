e58b382f169555ad985625a98254fb32a6800458
nileshpratap <nileshpratap03@gmail.com>
2023-03-31 20:43:22 +0530

    ...

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://contact-keeper-j10xlt0hg-nileshpratap.vercel.app"
+    "https://contact-keeper-omega.vercel.app/"
   ); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
