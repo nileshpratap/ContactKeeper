d65b1d502b6696c02c26d025293e7a0026a79e52
nileshpratap <nileshpratap03@gmail.com>
2023-03-31 17:51:21 +0530

    ..

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -1,10 +1,12 @@
 import axios from "axios";
-axios.defaults.baseURL = "https://coult.onrender.com";
+
 const setAuthToken = (token) => {
   if (token) {
-    axios.defaults.headers.common["x-auth-token"] = token.toString();
+    axios.defaults.headers.common["x-auth-token"] = token;
+    localStorage.setItem("token", token);
   } else {
     delete axios.defaults.headers.common["x-auth-token"];
+    localStorage.removeItem("token");
   }
 };
 
